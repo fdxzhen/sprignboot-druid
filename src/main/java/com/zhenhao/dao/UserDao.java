2a414077328173cf6fdf3fa8aadeaf53eb7c2596
zhenhao <18738989958@qq.com>
2019-11-25 15:13:47 +0800

    增加数据库查询的控制台输出

--- src/main/java/com/zhenhao/dao/UserDao.java
+++ src/main/java/com/zhenhao/dao/UserDao.java
@@ -3,10 +3,11 @@ package com.zhenhao.dao;
 
 import com.zhenhao.bean.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Mapper
 public interface UserDao{
 
     @Select("select * from user where id=#{id}")
